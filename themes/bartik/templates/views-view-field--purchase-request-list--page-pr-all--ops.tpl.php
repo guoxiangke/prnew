50b99025c7d268ed174dfeb811a4069f43365788
guo <guo@ylnwt.com>
2013-09-09 20:22:50 +0800

    Last Approval

--- themes/bartik/templates/views-view-field--purchase-request-list--page-pr-all--ops.tpl.php
+++ themes/bartik/templates/views-view-field--purchase-request-list--page-pr-all--ops.tpl.php
@@ -28,7 +28,7 @@
   // 5|Rejected
   // 6|Completed
   // 7|Cancelled
- dpm($row);
+ // dpm($row);
  $pr_node = node_load($row->nid);
  $show = array('4');
  if(in_array($pr_node->field_pr_status[LANGUAGE_NONE][0]['value'], $show)) {
